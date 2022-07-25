ccc72ee011ee4bea54442e2e6f213c56d4da220c
JessalynKemp <jessalyn1@btinternet.com>
2022-07-25 16:09:48 +0100

    write and style header component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { UserContext } from "./contexts/User";
 
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import ReviewList from "./components/ReviewList";
+import React from "react";
 
 function App() {
+  const [user, setUser] = useState({
+    username: "guestaccount",
+    name: "Guest Account",
+    avatar_url: "https://cdn-icons-png.flaticon.com/512/1077/1077114.png?w=360",
+  });
+
   return (
     <BrowserRouter>
-      <div className="App"></div>
-      <header>
-        <Header />
-        <NavBar />
-      </header>
-      <Routes>
-        <Route path="/" element={ReviewList} />
-      </Routes>
+      <UserContext.Provider value={user}>
+        <div className="app"></div>
+        <header>
+          <Header />
+          <NavBar />
+        </header>
+        <Routes>
+          <Route path="/" element={ReviewList} />
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,21 @@
-export default function Header () {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHouseChimney } from "@fortawesome/free-solid-svg-icons"
 
+import {Link} from "react-router-dom";
+
+import {useContext} from "react";
+import { UserContext } from "../contexts/User";
+
+export default function Header () {
+const user = useContext(UserContext);
+
+return (
+    <div className="header">
+        <div className="headerText">
+        <h1>Northcoders Game Reviews</h1>
+        <p>You are logged in as {user.name}</p>
+        </div>
+        <Link to="/"><FontAwesomeIcon id="homeIcon" icon={faHouseChimney} /></Link>
+    </div>
+)
 }
