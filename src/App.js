39d248c62107af04b86722e864007b164cf430c4
JessalynKemp <jessalyn1@btinternet.com>
2022-07-25 15:31:04 +0100

    setup component structure and router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {}
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import ReviewList from "./components/ReviewList";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App"></div>
+      <header>
+        <Header />
+        <NavBar />
+      </header>
+      <Routes>
+        <Route path="/" element={ReviewList} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
