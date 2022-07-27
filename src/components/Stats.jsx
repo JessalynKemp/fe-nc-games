db2479e2506739eb09786ddbebeaba1eec0830f7
JessalynKemp <jessalyn1@btinternet.com>
2022-07-27 11:26:11 +0100

    add voting buttons with error handling

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,19 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import { formatDateAndTime } from "../utilities/utilities";
 
-export default function Stats ({reviewData}) {
+export default function Stats ({reviewData, votes, setVotes}) {
 
 const {formattedDate, formattedTime} = formatDateAndTime(reviewData.created_at);
+const [error, setError] = useState("")
+
+function changeVote(num){
+    axios.patch(`https://nc-games-jk.herokuapp.com/api/reviews/${reviewData.review_id}`, {inc_votes: num}).then(({data}) => {
+        setError("");
+        setVotes(data.review.votes);
+    }).catch((err) => {
+        setError("Your vote did not apply, please try again.")
+    })
+}
+
 
 return <div className="statsBox">
     <p className="reviewCardOwner">{reviewData.owner}</p>
     <p className="reviewDateTime">{formattedTime} {formattedDate}</p>
     <div className="reviewVoteCount">
-        <p>Votes: {reviewData.votes}</p>
+        <p>Votes: {votes}</p>
         <div className="reviewVotingButtons">
-            <button>Up</button>
-            <button>Down</button>
+            <button className="votingButton" onClick={() => {changeVote(1)}}>Up</button>
+            <button className="votingButton" onClick={() => {changeVote(-1)}}>Down</button>
         </div>
     </div>
+    {error ? <p className="votingError">{error}</p> : <></>}
     <div className="reviewCategoryDesigner">
         <p>C: {reviewData.category}</p>
         <p>D: {reviewData.designer}</p>
